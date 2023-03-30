97b6e9a1e553a35f7e0f1b7de9e6dacf63f235ca
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-03-30 05:45:06 +0500

    🚀 initialized project, added get all and post API

---
+++ index.ts
@@ -0,0 +1,49 @@
+const express = require('express');
+const Joi = require('joi');
+
+const app = express();
+
+app.use(express.json());
+
+interface Genre {
+  id: number;
+  name: string;
+}
+
+const genres = [
+  { id: 1, name: 'Action' },
+  { id: 2, name: 'Horror' },
+  { id: 3, name: 'Romance' }
+];
+
+// Test route
+app.get('/', (req, res) => {
+  res.status(200).send('Server is up and running.');
+});
+
+// Get all genres route
+app.get('/api/genres', (req, res) => {
+  res.status(200).send(genres);
+});
+
+// Post genre route
+app.post('/api/genres', (req, res) => {
+  const { error } = validateGenre(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+  const genre = {
+    id: genres.length + 1,
+    name: req.body.name
+  };
+  genres.push(genre);
+  res.status(200).send(genre);
+});
+
+function validateGenre(genre) {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required()
+  });
+  return schema.validate(genre);
+}
+
+const PORT = 3000 || process.env.PORT;
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
