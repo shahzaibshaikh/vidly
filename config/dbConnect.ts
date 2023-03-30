98c6de85cb300b8410cd62bc80a4f87001604aa0
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-03-31 02:51:18 +0500

    🚀 added database connection function and .env

--- config/dbConnect.ts
+++ config/dbConnect.ts
@@ -2,6 +2,20 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
-const dbConnect = () => {};
+const dbConnect = () => {
+  const MONGO_DB_URL = process.env.MONGO_DB_URL;
+  if (!MONGO_DB_URL) {
+    console.error("MONGO_DB_URL is not defined in the environment variables.");
+    process.exit(1);
+  }
+  mongoose
+    .connect(MONGO_DB_URL)
+    .then((response) => {
+      console.log("MongoDB Connection Succeeded.");
+    })
+    .catch((error) => {
+      console.log("Error in DB connection: " + error);
+    });
+};
 
 export default dbConnect;
