d4330c2c4234040a7f38bba69d35674c3fcd2e9b
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-03-31 02:52:05 +0500

    👌 moved server port to .env

--- index.ts
+++ index.ts
@@ -13,5 +13,5 @@ app.use("/api/genres", genresRouter);
 
 dbConnect();
 
-const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
