e64266a1032ae668b5858aca7632558b348f0f06
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-03-31 22:53:06 +0500

    👌 modified delete and update routes

--- routes/genres.ts
+++ routes/genres.ts
@@ -5,16 +5,6 @@ import Genre from "../models/Genre";
 const router = express.Router();
 
 // interfaces
-interface Genre {
-  id: number;
-  name: string;
-}
-
-const genres = [
-  { id: 1, name: "Action" },
-  { id: 2, name: "Horror" },
-  { id: 3, name: "Romance" },
-];
 
 // Get all genres route
 router.get("/", async (req, res) => {
@@ -25,7 +15,7 @@ router.get("/", async (req, res) => {
 // Get specific genre route
 router.get("/:id", (req, res) => {
   const genre = Genre.findById(req.params.id);
-  if (!genre) res.status(404).send("Genre not found.");
+  if (!genre) return res.status(404).send("Genre not found.");
   return res.status(200).send(genre);
 });
 
@@ -43,30 +33,26 @@ router.post("/", async (req, res) => {
 });
 
 // Updating existing genre
-router.put("/:id", (req, res) => {
-  const genre: Genre | undefined = genres.find(
-    (genre) => genre.id === parseInt(req.params.id)
-  );
-  if (!genre) res.status(404).send("Genre does not exist.");
-
+router.put("/:id", async (req, res) => {
   const { error } = validateGenre(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
-  if (genre) {
-    genre.name = req.body.name;
-    res.status(200).send(genre);
-  }
+  const genre = await Genre.findByIdAndUpdate(
+    req.params.id,
+    { name: req.body.name },
+    { new: true }
+  );
+
+  if (!genre) res.status(404).send("Genre does not exist.");
+
+  res.status(200).send(genre);
 });
 
-router.delete("/:id", (req, res) => {
-  const genre = genres.find((genre) => genre.id === parseInt(req.params.id));
-  if (!genre) res.status(404).send("Genre not found.");
+router.delete("/:id", async (req, res) => {
+  const genre = await Genre.findByIdAndRemove(req.params.id);
+  if (!genre) return res.status(404).send("Genre not found.");
 
-  if (genre) {
-    const index = genres.indexOf(genre);
-    genres.splice(index, 1);
-    res.status(200).send("Genre deleted.");
-  }
+  res.status(200).send("Genre deleted.");
 });
 
 function validateGenre(genre) {
