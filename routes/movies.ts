8f784ad6c1e3bf4245add462cbe134dc2a5ea611
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-04-01 19:45:45 +0500

    ✅ correct post movie route

--- routes/movies.ts
+++ routes/movies.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import Genre from "../models/Genre";
 import Movie, { validateMovie } from "../models/Movie";
 
 const router = express.Router();
@@ -11,9 +12,9 @@ router.get("/", async (req, res) => {
 
 // Get specific movie route
 router.get("/:id", async (req, res) => {
-  const genre = await Movie.findById(req.params.id);
-  if (!genre) return res.status(404).send("Genre not found.");
-  return res.status(200).send(genre);
+  const movie = await Movie.findById(req.params.id);
+  if (!movie) return res.status(404).send("Movie not found.");
+  return res.status(200).send(movie);
 });
 
 // Post movie route
@@ -21,12 +22,21 @@ router.post("/", async (req, res) => {
   const { error } = validateMovie(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
-  let genre = new Movie({
-    name: req.body.name,
+  const genre = await Genre.findById(req.body.genreId);
+  if (!genre) return res.status(404).send("Genre not found.");
+
+  let movie = new Movie({
+    title: req.body.title,
+    genre: {
+      _id: req.body.genreId,
+      name: genre.name,
+    },
+    numberInStock: req.body.numberInStock,
+    dailyRentalRate: req.body.dailyRentalRate,
   });
 
-  genre = await genre.save();
-  res.status(200).send(genre);
+  movie = await genre.save();
+  res.status(200).send(movie);
 });
 
 // Updating existing movie
