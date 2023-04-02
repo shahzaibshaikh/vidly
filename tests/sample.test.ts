e25106fd0a23af060a414c225c5fe80185210f7a
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-04-02 06:10:50 +0500

    👌 removed test case suite 2

--- tests/sample.test.ts
+++ tests/sample.test.ts
@@ -5,11 +5,3 @@ describe("absolute", () => {
   test("Fourth test", () => {});
   test("Fifth test", () => {});
 });
-
-describe("absolute 2", () => {
-  test("First test", () => {});
-  test("Second test", () => {});
-  test("Third test", () => {});
-  test("Fourth test", () => {});
-  test("Fifth test", () => {});
-});
