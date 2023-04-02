ae7b9297ae8cb5e39baa1774093b5ae997103365
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-04-02 06:10:12 +0500

    👌 added some more test cases

--- tests/sample.test.ts
+++ tests/sample.test.ts
@@ -1,1 +1,15 @@
-test("First test", () => {});
+describe("absolute", () => {
+  test("First test", () => {});
+  test("Second test", () => {});
+  test("Third test", () => {});
+  test("Fourth test", () => {});
+  test("Fifth test", () => {});
+});
+
+describe("absolute 2", () => {
+  test("First test", () => {});
+  test("Second test", () => {});
+  test("Third test", () => {});
+  test("Fourth test", () => {});
+  test("Fifth test", () => {});
+});
